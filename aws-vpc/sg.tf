84b7659148d0b832916f032af077dca6f63f7a7a
권나영 <dek3127@gmail.com>
2023-03-10 15:08:14 +0900

    s3-backend tf 파일 main에 두기

--- aws-vpc/sg.tf
+++ aws-vpc/sg.tf
@@ -1,7 +1,14 @@
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+  name        = "naa0-k8s-sg"
+  description = "k8s용 보안그룹"
   vpc_id      = aws_vpc.main.id
+
+  ingress {
+		from_port       = 0
+		to_port         = 0
+		protocol        = "tcp"
+		cidr_blocks     = ["0.0.0.0/0"]
+	}
 
   egress {
     from_port        = 0
