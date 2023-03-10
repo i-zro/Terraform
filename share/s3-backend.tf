94dab4a8eca426bdef3897b04dee4595c6f67e07
권나영 <dek3127@gmail.com>
2023-03-10 15:08:14 +0900

    s3-backend tf 파일 main에 두기

--- share/s3-backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-      bucket         = "tf-naa0-backend" 
-      key            = "tf/s3/terraform.tfstate"
-      region         = "ap-northeast-2"
-      encrypt        = true
-      dynamodb_table = "tf-naa0-lock" 
-  }
-}
