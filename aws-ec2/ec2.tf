187f6c6446beb841190418b99dbd1ce97d5f0818
권나영 <dek3127@gmail.com>
2023-03-11 21:55:59 +0900

    ec2 3개 생성 확인

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -2,13 +2,15 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  for_each = toset(["k8s-master", "k8s-node1", "k8s-node2"])
 
-  ami                    = "ami-09eba584c30b7299f"
-  instance_type          = "t2.micro"
+  name = "instance-${each.key}"
+  
+	ami                    = "ami-09eba584c30b7299f"
+  instance_type          = "t2.medium"
   key_name               = "naa0-key"
   monitoring             = true
-  vpc_security_group_ids = ["sg-02190e8641b1284e3"] 
+  vpc_security_group_ids = ["sg-02190e8641b1284e3"]  
   subnet_id              = "subnet-08ce50e8a5d88d92b"
 
   tags = {
