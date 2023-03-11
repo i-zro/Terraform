52422762e6b585b2a5f911bc3de57f24fb4afdf4
권나영 <dek3127@gmail.com>
2023-03-11 22:05:05 +0900

    map_public_ip_on_launch = true  # 퍼블릭 ip 주소 자동 할당

--- aws-vpc/subnet.tf
+++ aws-vpc/subnet.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "first_subnet" {
   cidr_block = "10.1.1.0/24"
 
   availability_zone = "ap-northeast-2a"
+  map_public_ip_on_launch = "true"  # 퍼블릭 ip 주소 자동 할당
 
   tags = {
     Name = "subnet-tf-k8s-1"
@@ -14,6 +15,7 @@ resource "aws_subnet" "second_subnet" {
   cidr_block = "10.1.2.0/24"
 
   availability_zone = "ap-northeast-2b"
+  map_public_ip_on_launch = "true"  # 퍼블릭 ip 주소 자동 할당
 
   tags = {
     Name = "subnet-tf-k8s-2"
