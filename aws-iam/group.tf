b0adb3d6f3d6ac371fd7b27db639bcd070f4a703
권나영 <dek3127@gmail.com>
2023-04-03 19:24:43 +0900

    group에 iam user 할당

--- aws-iam/group.tf
+++ aws-iam/group.tf
@@ -1,3 +1,14 @@
 resource "aws_iam_group" "tf_temp_group" {
   name = "tf_temp_group"
+
+}
+
+resource "aws_iam_group_membership" "tf_temp" {
+  name = aws_iam_group.tf_temp_group.name
+
+	users = [
+    aws_iam_user.temp_naa0.name
+	]
+
+	group = aws_iam_group.tf_temp_group.name
 }
